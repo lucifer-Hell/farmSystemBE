0eb0ebc37c074d9bb5ccc98c197c424e0f1a20bf
lucifer-Hell <luciferhell58@gmail.com>
2023-07-01 21:05:22 +0530

    Attendence

---
+++ src/main/java/com/farmSystemBE/farmSystemBE/repository/AttendenceRepo.java
@@ -0,0 +1,7 @@
+package com.farmSystemBE.farmSystemBE.repository;
+
+import com.farmSystemBE.farmSystemBE.entity.Attendence;
+import org.springframework.data.repository.CrudRepository;
+
+public interface AttendenceRepo extends CrudRepository<Attendence,Long> {
+}
